cf7942634ea4f12d401f4cdf01da552e525e34e4
Jamiel <jamiel.van.engen@gmail.com>
2017-10-24 12:18:51 +0200

    Changed the variable password to token

--- src/main/java/nl/han/oose/dea/jamielvanengen/presentation/dtos/AuthorizedUser.java
+++ src/main/java/nl/han/oose/dea/jamielvanengen/presentation/dtos/AuthorizedUser.java
@@ -1,18 +1,23 @@
 package nl.han.oose.dea.jamielvanengen.presentation.dtos;
 
 public class AuthorizedUser {
-    private String password;
+    private String token;
     private String user;
 
+    public AuthorizedUser(String token, String user) {
+        this.token = token;
+        this.user = user;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
     public AuthorizedUser() {
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 
     public String getUser() {
