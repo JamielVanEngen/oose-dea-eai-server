dcfbf43b814f375c7e75110df559de0f4c7783d3
Jamiel <jamiel.van.engen@gmail.com>
2017-11-04 16:04:17 +0100

    Added the SongBuilder class and implemented the Builder method

---
+++ src/main/java/nl/han/oose/dea/jamielvanengen/domain/builders/SongBuilder.java
@@ -0,0 +1,28 @@
+package nl.han.oose.dea.jamielvanengen.domain.builders;
+
+import nl.han.oose.dea.jamielvanengen.domain.track.Track;
+import nl.han.oose.dea.jamielvanengen.domain.track.impl.Song;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SongBuilder implements Builder<Song> {
+    @Override
+    public List<Song> buildObjectFromResultSet(ResultSet resultSet) throws SQLException {
+        List<Song> songs = new ArrayList<>();
+        while (resultSet.next()) {
+            songs.add(new Song(
+                    resultSet.getInt("id"),
+                    resultSet.getString("performer"),
+                    resultSet.getString("titel"),
+                    resultSet.getString("url"),
+                    resultSet.getInt("afspeelduur"),
+                    resultSet.getObject( "playcount" , Integer.class ),
+                    resultSet.getBoolean("isAvailableOffline"),
+                    resultSet.getString("album")));
+        }
+        return songs;
+    }
+}
