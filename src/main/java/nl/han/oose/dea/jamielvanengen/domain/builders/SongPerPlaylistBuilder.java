f1e6503b0db9e556311604985494de31ef165ec1
Jamiel <jamiel.van.engen@gmail.com>
2017-11-04 17:09:50 +0100

    Changed the Song and VideoPerPlaylistBuilder classes to return a TrackPerPlaylist

--- src/main/java/nl/han/oose/dea/jamielvanengen/domain/builders/SongPerPlaylistBuilder.java
+++ src/main/java/nl/han/oose/dea/jamielvanengen/domain/builders/SongPerPlaylistBuilder.java
@@ -1,33 +1,30 @@
 package nl.han.oose.dea.jamielvanengen.domain.builders;
 
 import nl.han.oose.dea.jamielvanengen.domain.track.TrackPerPlaylist;
-import nl.han.oose.dea.jamielvanengen.domain.track.impl.Video;
+import nl.han.oose.dea.jamielvanengen.domain.track.impl.Song;
 
 import javax.enterprise.inject.Default;
 import javax.inject.Named;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 @Default
-@Named("VideoPerPlaylistBuilder")
-public class VideoPerPlaylistBuilder implements Builder<TrackPerPlaylist> {
-
+@Named("SongPerPlaylistBuilder")
+public class SongPerPlaylistBuilder implements Builder<TrackPerPlaylist> {
     @Override
     public List<TrackPerPlaylist> buildObjectFromResultSet(ResultSet resultSet) throws SQLException {
         List<TrackPerPlaylist> trackPerPlaylists = new ArrayList<>();
         while (resultSet.next()) {
-            trackPerPlaylists.add(new TrackPerPlaylist( new Video(
+            trackPerPlaylists.add(new TrackPerPlaylist( new Song(
                     resultSet.getInt("id"),
                     resultSet.getString("performer"),
                     resultSet.getString("titel"),
                     resultSet.getString("url"),
                     resultSet.getInt("afspeelduur"),
                     resultSet.getObject( "playcount" , Integer.class ),
-                    resultSet.getString("beschrijving"),
-                    resultSet.getObject( "publicatiedatum" , LocalDate.class )), resultSet.getBoolean("isAvailableOffline")));
+                    resultSet.getString("album")), resultSet.getBoolean("isAvailableOffline")));
         }
         return trackPerPlaylists;
     }
--- src/main/java/nl/han/oose/dea/jamielvanengen/domain/builders/TrackPerPlaylistBuilder.java
+++
@@ -1,4 +0,0 @@
-package nl.han.oose.dea.jamielvanengen.domain.builders;
-
-public class TrackPerPlaylistBuilder {
-}
