b39c584bcaba771c863a19753c94a37c7a4e4b1f
Jamiel <jamiel.van.engen@gmail.com>
2017-11-04 16:08:54 +0100

    Added the Builder<Video> implementation

---
+++ src/main/java/nl/han/oose/dea/jamielvanengen/domain/builders/VideoBuilder.java
@@ -0,0 +1,29 @@
+package nl.han.oose.dea.jamielvanengen.domain.builders;
+
+import nl.han.oose.dea.jamielvanengen.domain.track.impl.Video;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VideoBuilder implements Builder<Video> {
+    @Override
+    public List<Video> buildObjectFromResultSet(ResultSet resultSet) throws SQLException {
+        List<Video> songs = new ArrayList<>();
+        while (resultSet.next()) {
+            songs.add(new Video(
+                    resultSet.getInt("id"),
+                    resultSet.getString("performer"),
+                    resultSet.getString("titel"),
+                    resultSet.getString("url"),
+                    resultSet.getInt("afspeelduur"),
+                    resultSet.getObject( "playcount" , Integer.class ),
+                    resultSet.getBoolean("isAvailableOffline"),
+                    resultSet.getString("beschrijving"),
+                    resultSet.getObject( "publicatiedatum" , LocalDate.class )));
+        }
+        return songs;
+    }
+}
