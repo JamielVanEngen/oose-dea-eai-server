6608faef83a4d5c44599acc6a79f39763f9b9131
Jamiel <jamiel.van.engen@gmail.com>
2017-11-04 15:57:45 +0100

    Added a constructor and getters to the TrackPerPlaylist class

--- src/main/java/nl/han/oose/dea/jamielvanengen/domain/track/TrackPerPlaylist.java
+++ src/main/java/nl/han/oose/dea/jamielvanengen/domain/track/TrackPerPlaylist.java
@@ -1,4 +1,19 @@
 package nl.han.oose.dea.jamielvanengen.domain.track;
 
 public class TrackPerPlaylist {
+    private Track track;
+    private boolean isAvailableOffline;
+
+    public TrackPerPlaylist(Track track, boolean isAvailableOffline) {
+        this.track = track;
+        this.isAvailableOffline = isAvailableOffline;
+    }
+
+    public Track getTrack() {
+        return track;
+    }
+
+    public boolean isAvailableOffline() {
+        return isAvailableOffline;
+    }
 }
